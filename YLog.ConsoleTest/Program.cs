e5c843707d4fff6618dc73beb9f50ec524b2188b
Wenhao.Wu <wuwh@walkingtec.cn>
2016-10-19 16:39:26 +0800

    完成项目

---
+++ YLog.ConsoleTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YLog.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Debug("Debugger.Debug");
+            Debugger.Info("Debugger.Info");
+            Debugger.Warn("Debugger.Warn");
+            Debugger.Error("Debugger.Error");
+            Debugger.Fatal("Debugger.Fatal");
+
+
+
+            Trace.Debug("Trace.Debug");
+            Trace.Info("Trace.Info");
+            Trace.Warn("Trace.Warn");
+            Trace.Error("Trace.Error", new Exception("异常信息啊"));
+            Trace.Fatal("Trace.Fatal");
+            Trace.Write("Trace.Write");
+
+            Console.ReadLine();
+        }
+    }
+}
